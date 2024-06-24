cb8288425de81c563af68f6f08df35d99efc1bfe
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-06-24 21:37:31 +0200

    now password is searched in all combinations of chars string to the fixed length

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,17 @@ package org.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicReference;
 
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Main {
+    private static final AtomicReference<String> found = new AtomicReference<>(null);
     public static boolean checkIfPasswordIsRight(String password) {
         try {
-            ZipFile zip = new ZipFile("C:/Kacper_Ołdziejewski_CV.zip", password.toCharArray());
+            ZipFile zip = new ZipFile("C:/KacperCV.zip", password.toCharArray());
             if (zip.isEncrypted())
             {
                 zip.extractAll("/tmp");
@@ -21,7 +24,22 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void backtrack(String chars, String current, Integer maximalLen) {
+        System.out.println(current);
+        if (checkIfPasswordIsRight(current)) {
+            found.set(current);
+        }
 
+        if (current.length() < maximalLen) {
+            for (int i = 0; i < chars.length(); i++) {
+                backtrack(chars, current + chars.charAt(i), maximalLen);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        backtrack("abcd", "", 8);
+        System.out.println(found);
     }
 }
