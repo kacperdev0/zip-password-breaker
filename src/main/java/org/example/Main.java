c70a8d272f94c896eb3c99a4d207d31a9e6058f7
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-06-24 22:02:22 +0200

    now searching is executed on all processor threads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,18 @@ package org.example;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Main {
+    private static final ExecutorService executor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors() );
     private static final AtomicReference<String> found = new AtomicReference<>(null);
     public static boolean checkIfPasswordIsRight(String password) {
         try {
-            ZipFile zip = new ZipFile("C:/KacperCV.zip", password.toCharArray());
+            ZipFile zip = new ZipFile("C:/Users/kacpe/Desktop/TopSecret.zip", password.toCharArray());
             if (zip.isEncrypted())
             {
                 zip.extractAll("/tmp");
@@ -26,20 +28,26 @@ public class Main {
 
     public static void backtrack(String chars, String current, Integer maximalLen) {
         System.out.println(current);
+
         if (checkIfPasswordIsRight(current)) {
             found.set(current);
+            executor.shutdownNow();
+            return;
         }
 
-        if (current.length() < maximalLen) {
+        if (current.length() < maximalLen && found.get() == null) {
             for (int i = 0; i < chars.length(); i++) {
-                backtrack(chars, current + chars.charAt(i), maximalLen);
+               final String next = current + chars.charAt(i);
+               executor.execute(() -> backtrack(chars, next, maximalLen));
             }
         }
     }
 
 
     public static void main(String[] args) {
-        backtrack("abcd", "", 8);
-        System.out.println(found);
+        System.out.println(checkIfPasswordIsRight("abc"));
+        backtrack("abcde", "", 8);
+        while (executor.isTerminated() == false) {}
+        System.out.println("Password is: " + found);
     }
 }
