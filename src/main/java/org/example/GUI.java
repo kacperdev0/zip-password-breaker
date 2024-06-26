b86b32b4cbe243a24047d4d93315bf736e9d9f70
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-06-26 18:40:53 +0200

    added input for chars used in making combiantions

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -22,10 +22,12 @@ public class GUI {
     }
 
     public static void placeComponents(JPanel panel) {
+        // Set up GridBagConstraints for layout management
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 10, 10, 10);
 
+        // Button to select ZIP file
         JButton selectFile_Button = new JButton("Select ZIP");
         JFileChooser selectFile = new JFileChooser();
         selectFile.setFileFilter(new FileNameExtensionFilter("ZIP file", "zip"));
@@ -41,32 +43,52 @@ public class GUI {
             }
         });
 
+        // Add the selectFile_Button to the panel
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
         panel.add(selectFile_Button, gbc);
 
+        // Label for the maximum length input field
         JLabel length_Label = new JLabel("Max Length:");
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 2;
         panel.add(length_Label, gbc);
 
+        // Text field to input the maximum length
         JTextField length_TextField = new JTextField(20);
         length_TextField.setPreferredSize(new Dimension(200, 30));
-
+        length_TextField.setText("6");
         gbc.gridx = 2;
         gbc.gridy = 1;
         gbc.gridwidth = 2;
         panel.add(length_TextField, gbc);
 
-        JLabel result_Label = new JLabel("");
-
+        // Label for the characters to use input field
+        JLabel chars_Label = new JLabel("Chars to use:");
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        panel.add(chars_Label, gbc);
+
+        // Text field to input the characters to use
+        JTextField chars_TextField = new JTextField(20);
+        chars_TextField.setPreferredSize(new Dimension(200, 60));
+        chars_TextField.setText("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        panel.add(chars_TextField, gbc);
+
+        // Label to display the result of the password search
+        JLabel result_Label = new JLabel("");
+        gbc.gridx = 0;
+        gbc.gridy = 4;
         gbc.gridwidth = 2;
         panel.add(result_Label, gbc);
 
+        // Submit button to start the password search process
         JButton submit_Button = new JButton("Submit");
         submit_Button.addActionListener(new ActionListener() {
             @Override
@@ -75,8 +97,9 @@ public class GUI {
             }
         });
 
+        // Add the submit_Button to the panel
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         gbc.gridwidth = 2;
         panel.add(submit_Button, gbc);
     }
